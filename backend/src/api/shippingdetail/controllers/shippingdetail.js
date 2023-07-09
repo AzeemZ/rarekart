4c57b98b2429c637d129e9c1d46a75074ac95868
AzeemZ <azeem3z123@gmail.com>
2023-07-09 17:50:19 +0500

    add content-type builder

---
+++ backend/src/api/shippingdetail/controllers/shippingdetail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * shippingdetail controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::shippingdetail.shippingdetail');
---
+++ backend/src/api/shippingdetail/routes/shippingdetail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * shippingdetail router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::shippingdetail.shippingdetail');
