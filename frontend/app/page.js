8ffb88e0c9ef2f5b6818c626464f269a806f6696
AzeemZ <azeem3z123@gmail.com>
2023-07-13 01:23:35 +0500

    add header and other components

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,113 +1,176 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
+import Image from "next/image";
+import Link from "next/link";
+import Header from "@/components/Header";
+import RootLayout from "./layout";
+
+export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <RootLayout>
+      <Header />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at
+        placerat libero. Nunc elementum ante facilisis scelerisque ultricies.
+        Cras non nibh et risus euismod dignissim vel sed quam. Fusce lacus nisl,
+        semper iaculis mattis quis, aliquam pellentesque nisl. Vestibulum
+        aliquet, purus a tincidunt pharetra, massa velit ornare elit, nec dictum
+        est tellus ac enim. Ut ac tristique leo, non gravida elit. Etiam
+        placerat lorem in arcu interdum finibus. Integer dapibus nulla
+        tristique, tincidunt ligula at, molestie lorem. Nulla vehicula pharetra
+        condimentum. Suspendisse malesuada elit ullamcorper, volutpat lectus
+        quis, vehicula lorem. Integer ultricies lorem nec sapien faucibus
+        gravida. Fusce eleifend quam eget neque tincidunt consequat.
+      </p>
+      <p>
+        Curabitur quam risus, posuere id tempor et, rutrum vel libero. Nulla et
+        ex a nisi suscipit consequat id sed tellus. Sed tincidunt a sem id
+        hendrerit. Aenean pharetra tellus risus, quis sagittis lacus varius vel.
+        Sed mauris urna, molestie vitae vulputate ut, porttitor non ipsum.
+        Curabitur dolor augue, egestas ut hendrerit a, aliquet et ante. Nam
+        faucibus purus id dui tristique, sit amet dictum tellus pulvinar. Ut nec
+        suscipit nibh, posuere scelerisque dui. Nulla sollicitudin, sapien
+        tristique fringilla sollicitudin, ante neque viverra urna, eget commodo
+        ante risus ut metus. Nam ac eleifend ligula. Integer metus sem,
+        vulputate sit amet sapien vitae, imperdiet vehicula eros.
+      </p>
+      <p>
+        Quisque nec dignissim leo. Ut arcu dui, facilisis vel ante id, commodo
+        imperdiet orci. Etiam vulputate sagittis neque at elementum. Donec
+        bibendum nec enim quis iaculis. Vivamus nec felis velit. Ut semper eu
+        tortor id ornare. Donec non sollicitudin dolor, in aliquam nunc. Duis
+        pellentesque aliquam quam, eu fringilla tellus. Etiam tincidunt, nisi at
+        interdum finibus, augue nibh viverra leo, vitae vulputate sapien velit a
+        purus. Proin fermentum dictum volutpat.
+      </p>
+      <p>
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+        cubilia curae; Interdum et malesuada fames ac ante ipsum primis in
+        faucibus. Maecenas non consequat justo. Cras mi dui, egestas blandit
+        erat blandit, facilisis rhoncus felis. Maecenas vestibulum porttitor
+        felis nec cursus. In nec venenatis ante, vitae ullamcorper sapien. Proin
+        auctor est et laoreet tincidunt. Nam in bibendum metus.
+      </p>
+      <p>
+        Aliquam pharetra blandit mi, eu varius turpis vestibulum nec. Donec
+        imperdiet magna sed est finibus, sed fringilla tellus lacinia. Aliquam
+        molestie ex in nunc mollis, eu aliquet mi vestibulum. Aenean ullamcorper
+        nulla nisi, ut tempor elit tempor vel. Nullam sem lacus, vehicula in
+        porta sed, mattis sed nisl. Donec cursus odio vel elit ullamcorper
+        egestas. Etiam ultricies consequat ipsum. Nullam justo dolor, gravida ut
+        dolor eu, porttitor porttitor sapien. Orci varius natoque penatibus et
+        magnis dis parturient montes, nascetur ridiculus mus. Duis ultrices
+        lacus dolor, ac lacinia velit faucibus dictum. Nam placerat non elit id
+        sodales. Quisque in ex consectetur leo varius commodo.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at
+        placerat libero. Nunc elementum ante facilisis scelerisque ultricies.
+        Cras non nibh et risus euismod dignissim vel sed quam. Fusce lacus nisl,
+        semper iaculis mattis quis, aliquam pellentesque nisl. Vestibulum
+        aliquet, purus a tincidunt pharetra, massa velit ornare elit, nec dictum
+        est tellus ac enim. Ut ac tristique leo, non gravida elit. Etiam
+        placerat lorem in arcu interdum finibus. Integer dapibus nulla
+        tristique, tincidunt ligula at, molestie lorem. Nulla vehicula pharetra
+        condimentum. Suspendisse malesuada elit ullamcorper, volutpat lectus
+        quis, vehicula lorem. Integer ultricies lorem nec sapien faucibus
+        gravida. Fusce eleifend quam eget neque tincidunt consequat.
+      </p>
+      <p>
+        Curabitur quam risus, posuere id tempor et, rutrum vel libero. Nulla et
+        ex a nisi suscipit consequat id sed tellus. Sed tincidunt a sem id
+        hendrerit. Aenean pharetra tellus risus, quis sagittis lacus varius vel.
+        Sed mauris urna, molestie vitae vulputate ut, porttitor non ipsum.
+        Curabitur dolor augue, egestas ut hendrerit a, aliquet et ante. Nam
+        faucibus purus id dui tristique, sit amet dictum tellus pulvinar. Ut nec
+        suscipit nibh, posuere scelerisque dui. Nulla sollicitudin, sapien
+        tristique fringilla sollicitudin, ante neque viverra urna, eget commodo
+        ante risus ut metus. Nam ac eleifend ligula. Integer metus sem,
+        vulputate sit amet sapien vitae, imperdiet vehicula eros.
+      </p>
+      <p>
+        Quisque nec dignissim leo. Ut arcu dui, facilisis vel ante id, commodo
+        imperdiet orci. Etiam vulputate sagittis neque at elementum. Donec
+        bibendum nec enim quis iaculis. Vivamus nec felis velit. Ut semper eu
+        tortor id ornare. Donec non sollicitudin dolor, in aliquam nunc. Duis
+        pellentesque aliquam quam, eu fringilla tellus. Etiam tincidunt, nisi at
+        interdum finibus, augue nibh viverra leo, vitae vulputate sapien velit a
+        purus. Proin fermentum dictum volutpat.
+      </p>
+      <p>
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+        cubilia curae; Interdum et malesuada fames ac ante ipsum primis in
+        faucibus. Maecenas non consequat justo. Cras mi dui, egestas blandit
+        erat blandit, facilisis rhoncus felis. Maecenas vestibulum porttitor
+        felis nec cursus. In nec venenatis ante, vitae ullamcorper sapien. Proin
+        auctor est et laoreet tincidunt. Nam in bibendum metus.
+      </p>
+      <p>
+        Aliquam pharetra blandit mi, eu varius turpis vestibulum nec. Donec
+        imperdiet magna sed est finibus, sed fringilla tellus lacinia. Aliquam
+        molestie ex in nunc mollis, eu aliquet mi vestibulum. Aenean ullamcorper
+        nulla nisi, ut tempor elit tempor vel. Nullam sem lacus, vehicula in
+        porta sed, mattis sed nisl. Donec cursus odio vel elit ullamcorper
+        egestas. Etiam ultricies consequat ipsum. Nullam justo dolor, gravida ut
+        dolor eu, porttitor porttitor sapien. Orci varius natoque penatibus et
+        magnis dis parturient montes, nascetur ridiculus mus. Duis ultrices
+        lacus dolor, ac lacinia velit faucibus dictum. Nam placerat non elit id
+        sodales. Quisque in ex consectetur leo varius commodo.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at
+        placerat libero. Nunc elementum ante facilisis scelerisque ultricies.
+        Cras non nibh et risus euismod dignissim vel sed quam. Fusce lacus nisl,
+        semper iaculis mattis quis, aliquam pellentesque nisl. Vestibulum
+        aliquet, purus a tincidunt pharetra, massa velit ornare elit, nec dictum
+        est tellus ac enim. Ut ac tristique leo, non gravida elit. Etiam
+        placerat lorem in arcu interdum finibus. Integer dapibus nulla
+        tristique, tincidunt ligula at, molestie lorem. Nulla vehicula pharetra
+        condimentum. Suspendisse malesuada elit ullamcorper, volutpat lectus
+        quis, vehicula lorem. Integer ultricies lorem nec sapien faucibus
+        gravida. Fusce eleifend quam eget neque tincidunt consequat.
+      </p>
+      <p>
+        Curabitur quam risus, posuere id tempor et, rutrum vel libero. Nulla et
+        ex a nisi suscipit consequat id sed tellus. Sed tincidunt a sem id
+        hendrerit. Aenean pharetra tellus risus, quis sagittis lacus varius vel.
+        Sed mauris urna, molestie vitae vulputate ut, porttitor non ipsum.
+        Curabitur dolor augue, egestas ut hendrerit a, aliquet et ante. Nam
+        faucibus purus id dui tristique, sit amet dictum tellus pulvinar. Ut nec
+        suscipit nibh, posuere scelerisque dui. Nulla sollicitudin, sapien
+        tristique fringilla sollicitudin, ante neque viverra urna, eget commodo
+        ante risus ut metus. Nam ac eleifend ligula. Integer metus sem,
+        vulputate sit amet sapien vitae, imperdiet vehicula eros.
+      </p>
+      <p>
+        Quisque nec dignissim leo. Ut arcu dui, facilisis vel ante id, commodo
+        imperdiet orci. Etiam vulputate sagittis neque at elementum. Donec
+        bibendum nec enim quis iaculis. Vivamus nec felis velit. Ut semper eu
+        tortor id ornare. Donec non sollicitudin dolor, in aliquam nunc. Duis
+        pellentesque aliquam quam, eu fringilla tellus. Etiam tincidunt, nisi at
+        interdum finibus, augue nibh viverra leo, vitae vulputate sapien velit a
+        purus. Proin fermentum dictum volutpat.
+      </p>
+      <p>
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+        cubilia curae; Interdum et malesuada fames ac ante ipsum primis in
+        faucibus. Maecenas non consequat justo. Cras mi dui, egestas blandit
+        erat blandit, facilisis rhoncus felis. Maecenas vestibulum porttitor
+        felis nec cursus. In nec venenatis ante, vitae ullamcorper sapien. Proin
+        auctor est et laoreet tincidunt. Nam in bibendum metus.
+      </p>
+      <p>
+        Aliquam pharetra blandit mi, eu varius turpis vestibulum nec. Donec
+        imperdiet magna sed est finibus, sed fringilla tellus lacinia. Aliquam
+        molestie ex in nunc mollis, eu aliquet mi vestibulum. Aenean ullamcorper
+        nulla nisi, ut tempor elit tempor vel. Nullam sem lacus, vehicula in
+        porta sed, mattis sed nisl. Donec cursus odio vel elit ullamcorper
+        egestas. Etiam ultricies consequat ipsum. Nullam justo dolor, gravida ut
+        dolor eu, porttitor porttitor sapien. Orci varius natoque penatibus et
+        magnis dis parturient montes, nascetur ridiculus mus. Duis ultrices
+        lacus dolor, ac lacinia velit faucibus dictum. Nam placerat non elit id
+        sodales. Quisque in ex consectetur leo varius commodo.
+      </p>
+    </RootLayout>
+  );
 }
